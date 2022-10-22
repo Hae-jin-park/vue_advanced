a489fd1b34a8d3feea1939e825b614ab312b2064
Hae-jin-park <74353756+Hae-jin-park@users.noreply.github.com>
2022-10-22 20:59:42 +0900

    mixin

--- src/mixin/index.js
+++ src/mixin/index.js
@@ -1,1 +1,13 @@
-export default {};
+export default {
+  methods: {
+    toYmdHms(date) {
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, 0);
+      const _date = String(date.getDate()).padStart(2, 0);
+      const hours = String(date.getHours()).padStart(2, 0);
+      const minutes = String(date.getMinutes()).padStart(2, 0);
+      const seconds = String(date.getSeconds()).padStart(2, 0);
+      return `${year}-${month}-${_date} ${hours}:${minutes}:${seconds}`;
+    },
+  },
+};
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" class="mx-auto" src="../assets/logo.png" />
+    {{ wrapData }}
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import mixin from "@/mixin";
+
+export default {
+  mixins: [mixin],
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      toDay: new Date(),
+    };
+  },
+  computed: {
+    wrapData() {
+      return this.toYmdHms(this.toDay);
+    },
+  },
+};
+</script>
