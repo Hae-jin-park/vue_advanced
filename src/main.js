96f903dcc2ef4380d025ee461232f247ea7ccb66
Hae-jin-park <74353756+Hae-jin-park@users.noreply.github.com>
2022-11-05 23:40:14 +0900

    vuetify

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import "./output.css";
 import router from "./router";
 import "tw-elements";
 import store from './store'
+import vuetify from './plugins/vuetify'
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App)
 }).$mount("#app");
---
+++ src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    ID : <input type="text" v-model="id" /><br />
+    PASSWORD : <input type="text" v-model="pw" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: "",
+      pw: "",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
